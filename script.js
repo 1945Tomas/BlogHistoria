6358ce26bca3fb5e35f081f7ffbadbebca0dfd36
TomiSayus <tomisayus.95@gmail.com>
2023-10-09 16:00:23 -0300

    query 1.0

--- script.js
+++ script.js
@@ -1,33 +1,5 @@
-// const datos = [{
-//     img : "/multimedia/notas/mapa versalles.jpg",
-//     descp: "Versalles"
-// }]
-
-// console.log(datos)
-// let notas = document.querySelector("#notas")
-// notas.style.background = "green";
-// notas.style.height = "400px"; 
-
-// function dinamicasNotas (imgSrc, descp) {
-// let div = document.createElement("div");
-// let imagen = document.createElement("img");
-// imagen.src = imgSrc;
-// let p = document.createElement("p");
-// p.innerHTML = desc;
-// div.appendChild(img);
-// div.appendChild(p);
-// notas.appendChild(div);
-// }
-// //forEach ( psra cada dato )
-// datos.forEach (({imgSrc,descp}) => dinamicasNotas(imgSrc,descp))
-
-
-
-
-
 
 //scrool en pagina //
-
 function scrollToElement(elementSelector, instance = 0) {
     // Select all elements that match the given selector
     const elements = document.querySelectorAll(elementSelector);
@@ -37,7 +9,6 @@ function scrollToElement(elementSelector, instance = 0) {
         elements[instance].scrollIntoView({ behavior: 'smooth' });
     }
 }
-
 document.addEventListener('DOMContentLoaded', function () {
     const link1 = document.getElementById("link1");
     const link2 = document.getElementById("link2");
